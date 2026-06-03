db9a80adeedd4512abc99dd3431371401bdeecde
Hadi Almubarak <hadi.almubarak3@gmail.com>
2026-06-03 07:11:41 +0300

    chore: set up Tailwind v4, remove Vite scaffold

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-surface text-text">
+      <p className="p-8 text-center">KFU Scheduler</p>
+    </div>
+  )
+}
+
+export default App
