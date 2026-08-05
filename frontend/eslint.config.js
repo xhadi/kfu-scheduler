ffcc39594336c62f1106ee357775f5053a6ae66a
Hadi Almubarak <hadi.almubarak3@gmail.com>
2026-08-05 18:44:21 +0300

    chore(frontend): update config files and add secondary color tokens

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -18,7 +18,7 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
     rules: {
-      'react-refresh/only-export-components': ['warn', { allowExportNames: ['useTheme', 'useLanguage', 'useSchedule', 'useCourseCatalog'] }],
+      'react-refresh/only-export-components': ['warn', { allowExportNames: ['useTheme', 'useUiText', 'useSchedule', 'useCourseCatalog'] }],
     },
   },
 ])
